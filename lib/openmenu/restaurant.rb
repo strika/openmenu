12313942fcd7046cbf84f30a72e2db623e4768cb
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2013-01-18 09:38:27 +0100

    Renamed Api to Restaurant.

--- lib/openmenu/restaurant.rb
+++ lib/openmenu/restaurant.rb
@@ -2,8 +2,8 @@ require 'net/http'
 require 'openmenu'
 
 module OpenMenu
-  class Api
-    def self.get_restaurant(id)
+  class Restaurant
+    def self.find(id)
       uri = URI.parse("http://openmenu.com/menu/#{id}")
       response = Net::HTTP.get(uri)
       OpenMenu.parse(response)
--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -1,10 +1,10 @@
 require "test/unit"
 require "openmenu"
 
-class ApiTest < Test::Unit::TestCase
-  def test_get_restaurant
+class RestaurantTest < Test::Unit::TestCase
+  def test_find
     @open_menu_id = "20e54732-15bb-11e0-b40e-0018512e6b26"
-    @fiorini = OpenMenu::Api.get_restaurant(@open_menu_id)
+    @fiorini = OpenMenu::Restaurant.find(@open_menu_id)
 
     assert_equal "Fiorini", @fiorini.restaurant_name
     assert_equal "209 E. 56th St.", @fiorini.address_1
