8d0f54acf0edf849b8f164d5c6e8a27b26b958ad
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2013-02-05 11:05:11 +0100

    Calling to_s on OpenMenu response.

--- lib/openmenu/restaurant.rb
+++ lib/openmenu/restaurant.rb
@@ -5,7 +5,7 @@ module OpenMenu
   class Restaurant
     def self.find(id)
       uri = URI.parse("http://openmenu.com/menu/#{id}")
-      response = Net::HTTP.get(uri)
+      response = Net::HTTP.get(uri).to_s
       OpenMenu.parse(response)
     end
   end
